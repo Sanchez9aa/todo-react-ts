8c6d663719205102fbcfa2c47272d083567a4c01
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-12-15 00:05:52 +0100

    Create todoInput.style.js

---
+++ src/components/todoInput/todoInput.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const TodoInputStyle = styled.input`
+    background: white;
+    padding: 20px;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+`
+
+export const AddTask = styled.button`
+    background: #4B61D1;
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    border-radius: 50%;
+    box-shadow: 0 0 1px 0 #ccc;
+    right: 5px;
+`
