b7736942bc204c3ec4c0288cbfb60c25b1b14187
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-15 15:13:27 +0100

    Update ToDoInput.tsx

--- src/components/todoInput/ToDoInput.tsx
+++ src/components/todoInput/ToDoInput.tsx
@@ -1,14 +1,31 @@
-import React, { FC } from 'react'
-import { TodoInputStyle, AddTask } from './todoInput.style'
+import React, { FC, useRef } from 'react'
+import { TodoInputStyle, AddTask, TodoDiv } from './todoInput.style'
+import {Props} from './model'
 
-const ToDoInput = () => {
+const ToDoInput: FC<Props> = ({task, setTask, setTasks, tasks}) => {
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setTask(e.target.value)
+    }
+
+    const handleAdd = (e:React.FormEvent): void => {
+        e.preventDefault()
+        input.current?.blur()
+        if(task){
+            setTasks([ ...tasks, {id: Date.now(), task, isDone: false}])
+            setTask("")
+        }
+    }
+
+    const input = useRef<HTMLInputElement>(null)
+
     return (
-        <>
-            <TodoInputStyle />
-            <AddTask>
-                Go
+        <TodoDiv>
+            <TodoInputStyle ref={input} placeholder="Enter a task" onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)}/>
+            <AddTask onClick={(e:React.FormEvent) => handleAdd(e)}>
+                Add
             </AddTask>
-        </>
+        </TodoDiv>
     )
 }
 
