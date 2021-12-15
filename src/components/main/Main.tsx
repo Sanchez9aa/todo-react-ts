bf5b06e92fd60f3cf074fd47ef58dc8d89f2bb90
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-15 15:13:49 +0100

    Update Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,18 +1,26 @@
 import React, { FC, useState } from 'react';
 import { MainBody, MainTitle, GlobalStyles } from './main.styles'
 import ToDoInput from '../todoInput/ToDoInput';
+import {TaskModel} from '../taskList/model'
+import Task from '../taskList/TaskList';
 
-const Main = () => {
-    return (
-      <>
+
+const Main: FC = () => {
+
+  const [task, setTask] = useState<string>("")
+  const [tasks, setTasks] = useState<TaskModel[]>([])
+
+  return (
+    <>
       <GlobalStyles />
-        <MainBody>
-          <MainTitle>To Do</MainTitle>
-          <ToDoInput />
-        </MainBody>
-      
-      </>
-    );
-  }
+      <MainBody>
+        <MainTitle>To Do</MainTitle>
+        <ToDoInput task={task} setTask={setTask} setTasks={setTasks} tasks={tasks} />
+        <Task tasks={tasks} />
+      </MainBody>
+
+    </>
+  );
+}
 
 export default Main
