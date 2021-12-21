8dd67b93c260c52e4648b6201a4e14d606d3284b
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-21 18:31:11 +0100

    Update TaskList.tsx

--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -1,18 +1,29 @@
 import React, { FC } from 'react'
 import { TaskModel } from './model'
-import {StyledTask} from './TaskList.style'
+import { StyledTask } from './TaskList.style'
 import Task from '../task/Task'
 
 interface Tasks {
-  tasks: TaskModel[]
+  tasks: TaskModel[],
+  setTasks: any,
 }
 
-const TaskList: FC<Tasks> = ({tasks}) => {
- return (
-  <StyledTask>
-    {tasks.map(x => <Task key={x.id} x={x}/>)}
-  </StyledTask>
- )
+const TaskList: FC<Tasks> = ({ tasks, setTasks }) => {
+
+  const handleIsDone = (id: number) => {
+    console.log()
+    setTasks(
+      tasks.map((task: TaskModel) => 
+        task.id === id ? {...task, isDone: !task.isDone} : task
+      )
+    )
+  }
+  return (
+    <StyledTask>
+      {console.log(tasks)}
+      {tasks.map(x => <Task key={x.id} task={x} handleIsDone={handleIsDone}/>)}
+    </StyledTask>
+  )
 }
 
 export default TaskList
