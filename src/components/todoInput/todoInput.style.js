384e08db74aa2382256cdefc28b2aabc1b602f0f
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-22 13:47:55 +0100

    Update todoInput.style.js

--- src/components/todoInput/todoInput.style.js
+++ src/components/todoInput/todoInput.style.js
@@ -4,43 +4,42 @@ export const TodoDiv = styled.div`
     display: flex;
     width: 100%;
     padding: 20px;
-    position: fixed;
     justify-content: center;
     align-items:center;
-`
-export const TodoInputStyle = styled.input`
-    background: transparent;
-    padding: 20px;
-    position: fixed;
-    display: flex;
-    justify-content: center;
-    width: 70%;
-    height: 43px;
-    border: none;
-    border-bottom: 1px solid white;
-    border-radius: 15px;
-    color:white;
-    &:focus{
-        outline: none;
-    }
-`
 
-export const AddTask = styled.button`
-    background: #4B61D1;
-    color: white;
-    font-size: 20px;
-    position: absolute;
-    border-radius: 10px;
-    box-shadow: 0 0 1px 0 #ccc;
-    right: 15%;
-    border: none;
-    padding: 10px;
-    cursor: pointer;
-    transition: 0.7s linear;
+    input {
+			flex: 2;
+    	padding: 20px;
+    	display: flex;
+    	height: 40px;
+    	border: none;
+    	border-radius: 15px;
+    	color:black;
+			margin: 0 20px;
+			max-width: 400px;
+    &:focus{
+      outline: none;
+    }
+    }
+
+    button{
+			flex: 1;
+			max-width: 70px;
+      background: #4B61D1;
+    	color: white;
+    	font-size: 20px;
+    	border-radius: 10px;
+    	box-shadow: 0 0 1px 0 #ccc;
+    	border: none;
+    	padding: 10px;
+    	cursor: pointer;
+    	transition: 0.7s linear;
+			height: 40px;
     &:hover{
-        transform: scale(1.1)
+      transform: scale(1.1)
     }
     &:active{
-        transform: translateY(5px);
+      transform: translateY(5px);
+    }
     }
 `
