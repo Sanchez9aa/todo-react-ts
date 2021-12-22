33dd77e3b4ddcef11dcd33b1626dc842334ee0ac
Jesus Sanchez <74028565+Sanchez9aa@users.noreply.github.com>
2021-12-22 13:47:42 +0100

    Update task.style.js

--- src/components/task/task.style.js
+++ src/components/task/task.style.js
@@ -1,7 +1,41 @@
 import styled from 'styled-components'
 
 export const StyledSingleTask = styled.div`
-  background: black;
+  background: #FFBD35;
   font-size: 24px;
-  color:white
+  color:#111;
+  padding: 10px 20px;
+  margin: 15px 10px;
+  display:flex;
+  justify-content:space-evenly;
+  align-center:center;
+  max-width: 1100px;
+  border-radius: 10px;
+  width: 100%;
+
+  span{
+    font-size: 21px;
+    margin: 0 5px;
+    flex: 2;
+  }
+
+  s{
+    margin: 0 5px;
+    font-size: 21px;
+    flex: 2;
+  }
+
+`
+export const SvgWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex: 1;
+
+  > svg{
+    margin: auto 5px;
+    color: #333;
+    cursor: pointer;
+    font-size: 17px;
+  }
 `
