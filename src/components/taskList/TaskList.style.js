67474462ccfa610d984414e577537862abc16827
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-12-27 01:48:33 +0100

    Update TaskList.style.js

--- src/components/taskList/TaskList.style.js
+++ src/components/taskList/TaskList.style.js
@@ -2,8 +2,10 @@ import styled from 'styled-components'
 
 export const StyledTask = styled.div`
   display:flex;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
   flex-wrap: wrap;
   max-width: 1100px;
+  flex-direction: column;
+  margin: auto;
 `
